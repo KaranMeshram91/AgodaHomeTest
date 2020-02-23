8ef72feb14d404fe66fac5d747884844c0f3ec0c
Karan Meshram <karan.meshram@D11-KaranMeshram.local>
2020-02-23 14:02:01 +0530

    added Documentation and logical comments for code understanding

--- TestPasswords/src/main/java/com/changepassword/service/PasswordChanger.java
+++ TestPasswords/src/main/java/com/changepassword/service/PasswordChanger.java
@@ -10,16 +10,34 @@ import java.util.regex.Pattern;
 public class PasswordChanger {
 
     private static final Logger logger = LogManager.getLogger(PasswordChanger.class);
+    /*below regex checks for
+    *(?!\d+$) - upfront check that not all characters are digit
+    * [a-zA-Z0-9!@#$&*] should start with atleast lower case or upper case or a digit or allowed special characters !@#$&*
+    * [a-zA-Z0-9!@#$&*]* Any number of repeatition of lower case or upper case or a digit or allowed special characters !@#$& */
     private static Pattern allowedCharPattern = Pattern.compile("^(?!\\d+$)([a-zA-Z0-9!@#$&*][a-zA-Z0-9!@#$&*]*)$");
+    /*Below regex checks that
+    * (?=.*[a-z]) should contain atleast 1 lower case
+    * (?=.*[A-Z]) should contain atleast 1 upper case
+    * (?=.*\d) should contain atleast 1 digit
+    * (?=.*([^\w])) should contain atleast 1 special symbol
+    * +$ Match upto the end of string*/
     private static Pattern allowedLenghtPattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*([^\\w])).+$");
+
+    //created all the constants for various checks that can be changed in the future easily
     private static final int PASSWORD_MIN_LENGTH = 18;
     private static final int CHAR_REPEAT_LENGTH = 4;
     private static final int DIGIT_ALLOWED_PERCENT = 50;
     private static final int ALLOWED_MATCH_PERCENT = 80;
     private static final int SPECIAL_CHAR_REPEAT_LENGTH = 4;
+
+    //Mock boolean variable to check if old password matches with the system true/false
     private static boolean oldPasswordSystemCheck;
 
-
+    /*Main function to check if change password is allowed
+    * Checks 3 conditions and returns true if all the 3 of them are satisfied
+    * 1) verifyOldPassword - checks old password with system
+    * 2) validateNewPassword - validated if the new password is as per the rules defined
+    * 3) withinMatchPercentLimit - if the new password and old password are within the match % limit defined */
     public static boolean changePassword(String oldPassword, String newPassword) {
         logger.info("In Change password for Old Password : {} and New Password : {}",oldPassword,newPassword);
         System.out.println("In Change password for Old Password :" +oldPassword + " and New Password :" + newPassword);
@@ -44,24 +62,36 @@ public class PasswordChanger {
 
     }
 
-
+    /**
+     *
+     * @param newPassword - is the password to be validated as per defined rules
+     * @return true if all the rules satisfied false otherwise
+     */
     private static boolean validateNewPassword(String newPassword) {
         boolean isValid = false;
+        //check new password is not null and non empty
         if (null != newPassword && !("".equals(newPassword))) {
             int newPasswordLength = newPassword.length();
+            /*check the new password satisfies
+            * 1) The minimum length requirement
+            * 2) Check for conditions - At least 18 alphanumeric characters and list of special chars !@#$& and At least 1 Upper case, 1 lower case ,least 1 numeric, 1 special character*/
             if (newPasswordLength >= PASSWORD_MIN_LENGTH && isAlphaNumeric(newPassword) ) {
+                //Map to store the counts of each char in the new password
                 Map<Character, Integer> passwordCharCount = new HashMap<Character, Integer>();
                 int allowedSpecialCharCount = 0;
                 int allowedDigitCount = 0;
                 float digitPercentage = 0;
-                boolean continueLoop = true; //change name
+                //boolean to check if the loop breaks if any one of the allowed new password rule breaks
+                boolean continueLoop = true;
                 for (int i = 0; i < newPasswordLength && continueLoop; i++) {
                     char thisChar = newPassword.charAt(i);
                     Integer charCount = passwordCharCount.get(thisChar);
+                    // if a char is encountered for the first time
                     if (null == charCount) {
                         passwordCharCount.put(thisChar, 1);
                     } else {
                         charCount++;
+                        //if char count is > that the allowed char repeat length break
                         if (charCount > CHAR_REPEAT_LENGTH) {
                             continueLoop = false;
                             System.out.println(thisChar + " Repeate length exceeded : " + CHAR_REPEAT_LENGTH);
@@ -71,6 +101,7 @@ public class PasswordChanger {
                             passwordCharCount.put(thisChar, charCount);
                         }
                     }
+                    // if the char is digit track its count to check the Digit allowed % is not breached
                     if (Character.isDigit(thisChar)) {
                         allowedDigitCount += 1;
                         digitPercentage = (float) (allowedDigitCount * 100) / newPasswordLength;
@@ -81,7 +112,9 @@ public class PasswordChanger {
                             System.out.println(thisChar + " Digit percent equal or exceeded : " + DIGIT_ALLOWED_PERCENT);
                             logger.error( "{} Digit percent equal or exceeded : {} ",thisChar,DIGIT_ALLOWED_PERCENT);
                         }
-                    } else if (!(thisChar >= 'A' && thisChar <= 'Z') && !(thisChar >= 'a' && thisChar <= 'z')) {
+                    }
+                    // special characters count to be tracked to see if they do not exceed special character allowed repeat length
+                    else if (!(thisChar >= 'A' && thisChar <= 'Z') && !(thisChar >= 'a' && thisChar <= 'z')) {
                         allowedSpecialCharCount++;
                         /*System.out.println("Allowed Special character count : "+ allowedSpecialCharCount);
                         logger.debug("Allowed Special character count : {}",allowedSpecialCharCount);*/
@@ -92,12 +125,14 @@ public class PasswordChanger {
                         }
                     }
                 }
+                //some loggers and console output for debugging
                 System.out.println("Digit % : "+ digitPercentage + " : " + allowedDigitCount+"/"+newPasswordLength);
                 logger.debug("Digit % : "+ digitPercentage + " : " + allowedDigitCount+"/"+newPasswordLength);
                 System.out.println("Allowed Special character count : "+ allowedSpecialCharCount);
                 logger.debug("Allowed Special character count : {}",allowedSpecialCharCount);
                 System.out.println(passwordCharCount.toString());
                 logger.debug(passwordCharCount.toString());
+                //if the loop never breaks then all checks on new password has passed
                 if (continueLoop) {
                     isValid = true;
                 }
@@ -118,9 +153,16 @@ public class PasswordChanger {
         return isValid;
     }
 
+    /**
+     *
+     * @param oldPassword - old password to be changed
+     * @param newPassword - new password sent by calling function
+     * @return - true if the match % as per the Edit distance is within allowed limit else false
+     */
     private static boolean withinMatchPercentLimit(String oldPassword, String newPassword) {
         boolean isAllowedMatchPercent = false;
         int editDistance = EditDistance.editDistDP(oldPassword, newPassword, oldPassword.length(), newPassword.length());
+        //finding the larger of the old and new password for % match calculation
         int largerStringLength = 0;
         if (oldPassword.length() >= newPassword.length()) {
             largerStringLength = oldPassword.length();
@@ -138,18 +180,34 @@ public class PasswordChanger {
         return isAllowedMatchPercent;
     }
 
+    /**
+     * Helper for mock function
+     * @return true if system old password matched the given old password
+     */
     public static boolean oldPasswordSystemCheck() {
         return oldPasswordSystemCheck;
     }
 
+    // to set the old password mock check to true or false for old password test conditions
     public static void setOldPasswordSystemCheck(boolean oldPasswordSystemCheck) {
         PasswordChanger.oldPasswordSystemCheck = oldPasswordSystemCheck;
     }
 
+    /**
+     * Mock function for old password system check
+     * @return true if old password matches with the System else false
+     */
     private static boolean mockOldPasswordSystemCheck() {
         return oldPasswordSystemCheck();
     }
 
+    /**
+     * Function to check 2 conditions
+     * 1. At least 18 alphanumeric characters and list of special chars !@#$&*
+     * 2. At least 1 Upper case, 1 lower case ,least 1 numeric, 1 special character
+     * @param password takes the new password
+     * @return true if both the above conditions 1 and 2 satisfy by regex matching
+     */
     public static boolean isAlphaNumeric(String password) {
         System.out.println("in is alpha numeric and allowed special character check");
         logger.info("in is alpha numeric and allowed special character check");
